dbb5b772f05ae7f631781596ca1713cecf9bcbb7
Leah Kemp <35628281+UwUtisum@users.noreply.github.com>
2024-10-24 07:47:46 +0100

    updated to new site layout

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-function openCity(evt, cityName) {
-  var i, tabcontent, tab;
-	
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-	
-  tab = document.getElementsByClassName("tab");
-  for (i = 0; i < tab.length; i++) {
-    tab[i].className = tab[i].className.replace(" active", "");
-  }
-	
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-	
-}
+/* Original design by Sebastian Petravic -> https://dribbble.com/shots/3700168-Scotch-VHS */
